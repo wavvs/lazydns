07a8dbfb65896a41a579eacbd563d8efd0d18ccf
wavvs <30052411+wavvs@users.noreply.github.com>
2020-08-15 21:40:54 +0300

    Add dnssec test

--- publicdns.py
+++ publicdns.py
@@ -7,9 +7,9 @@ PUBDNS_URL = 'https://public-dns.info/nameservers.csv'
 def fetch_resolvers():
     r = requests.get(PUBDNS_URL).content.decode().split('\n')
     data = csv.DictReader(r)
-    valid_resolvers = [i['ip_address'] for i in data if i['reliability'] == '1.00' and ':' not in i['ip_address']]
-    for i in valid_resolvers:
-        print(i)
+    for i in data:
+        if i['reliability'] == '1.00' and i['dnssec'] == 'true': 
+            print(i['ip_address'])
 
 
 if __name__ == '__main__':
