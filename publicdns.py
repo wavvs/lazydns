9229b935046a2034adf58deec3a0b93a32b38300
wavvs <30052411+wavvs@users.noreply.github.com>
2020-09-14 00:12:54 +0300

    Add custom validation of resolvers using massdns

--- publicdns.py
+++ publicdns.py
@@ -1,5 +1,7 @@
 import requests
 import csv
+import random
+import string
 
 PUBDNS_URL = 'https://public-dns.info/nameservers.csv'
 
@@ -8,9 +10,11 @@ def fetch_resolvers():
     r = requests.get(PUBDNS_URL).content.decode().split('\n')
     data = csv.DictReader(r)
     for i in data:
-        if i['reliability'] == '1.00' and i['dnssec'] == 'true': 
+        if i['reliability'] == '1.00' and i['dnssec'] == 'true':
             print(i['ip_address'])
 
 
-if __name__ == '__main__':
-    fetch_resolvers()
+def gen_nxdomain(num, domain):
+    for _ in range(num):
+        subdomain = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
+        print(subdomain + '.' + domain)
